5a214677e413986be87c6c37694c566547967ff7
Sarvar M <sarvar.m149@gmail.com>
2022-12-03 00:29:49 +0700

    solution in function.js file

--- functions.js
+++ functions.js
@@ -1,10 +1,27 @@
-const getProductsOfNormalQuality = () => {};
+const getProductsOfNormalQuality = (pArray) => {
+  let result = pArray.filter((obj) => obj.quality == "Normal");
+  return result;
+};
 
-const getProductImageWithPngExtension = () => {};
+const getProductImageWithPngExtension = (pArray) => {
+  let result = pArray.filter((obj) => obj.productImage.slice(-3) == "png");
+  return result;
+};
 
-const getCalorieOfTheMostExpensiveProduct = () => {};
+const getCalorieOfTheMostExpensiveProduct = (pArray) => {
+  let maxPrice = Math.max(...pArray.map((obj) => obj.price));
+  let result = pArray
+    .filter((obj) => obj.price == maxPrice)
+    .map((obj) => obj.totalCalories)[0];
+  return result;
+};
 
-const sortByExpirationDate = () => {};
+const sortByExpirationDate = (pArray) => {
+  let result = pArray.sort(
+    (a, b) => a.expireDate.getTime() - b.expireDate.getTime()
+  );
+  return result;
+};
 
 export {
   getProductsOfNormalQuality,
--- functions.test.js
+++ functions.test.js
@@ -1,44 +1,44 @@
-import { productList } from './data.js';
+import { productList } from "./data.js";
 import {
   getProductsOfNormalQuality,
   getProductImageWithPngExtension,
   getCalorieOfTheMostExpensiveProduct,
   sortByExpirationDate,
-} from './functions.js';
+} from "./functions.js";
 
-test('', () => {
+test("", () => {
   let result = getProductsOfNormalQuality(productList);
   expect(result.length).toBe(5);
 });
 
-test('', () => {
+test("", () => {
   let result = getProductsOfNormalQuality(productList);
-  expect(result[1].productName).toBe('Waldbeeren');
+  expect(result[1].productName).toBe("Waldbeeren");
 });
 
-test('', () => {
+test("", () => {
   let result = getProductImageWithPngExtension(productList);
   expect(result.length).toBe(3);
 });
 
-test('', () => {
+test("", () => {
   let result = getProductImageWithPngExtension(productList);
   expect(result[0].productImage).toBe(
-    'https://image.migros.ch/product-zoom/7155c76b6f1134f6a57795ded7c1fb8c80953cf7/extra-konfituere-hagenbutten.png'
+    "https://image.migros.ch/product-zoom/7155c76b6f1134f6a57795ded7c1fb8c80953cf7/extra-konfituere-hagenbutten.png"
   );
 });
 
-test('', () => {
+test("", () => {
   let result = getCalorieOfTheMostExpensiveProduct(productList);
   expect(result).toBe(250);
 });
 
-test('', () => {
+test("", () => {
   let result = sortByExpirationDate(productList);
-  expect(result[0].productName).toBe('Erdbeer Klein');
+  expect(result[0].productName).toBe("Erdbeer Klein");
 });
 
-test('', () => {
+test("", () => {
   let result = sortByExpirationDate(productList);
-  expect(result[result.length - 1].productName).toBe('Aprikosen');
+  expect(result[result.length - 1].productName).toBe("Aprikosen");
 });
